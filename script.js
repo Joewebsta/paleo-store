4417068272d0d74c2fd106b7a32f3a7223fc6390
Joe Webster <joseph.scott.webster@gmail.com>
2020-07-07 12:44:57 -0400

    Create updateDisplay, fetchBlob and showProduct functions

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-fetch('products.json').then((response) => {
+fetch('products.json').then( response => {
   return response.json();
-}).then((json) => {
+}).then( json => {
   const products = json;
   initialize(products);
-}).catch((error) => {
+}).catch( error => {
   console.log(`Fetch error: ${error.message}`)
 })
 
@@ -12,29 +12,49 @@ function initialize(products) {
   const searchTerm = document.querySelector('#searchTerm');
   const productContainer = document.querySelector('.product-container');
 
-  const selectedMacro = macroSelect.value;
+  let finalGroup;
+  // finalGroup = products; WHY EXACTLY DO I NEED TO DO THIS?
+  updateDisplay();
 
-  function displayProducts(products) {
 
+  function updateDisplay() {
     products.forEach(product => {
-      const section = document.createElement('section');
-      const h2 = document.createElement('h2');
-      const p = document.createElement('p');
-      const img = document.createElement('img');
-
-      section.className = product.type;
-      h2.textContent = product.name;
-      p.textContent = `$${product.price}`;
-      img.src = `images/${product.image}`
-
-      productContainer.append(section);
-      section.append(h2);
-      section.append(p);
-      section.append(img);
+      fetchBlob(product);
     })
   }
 
-  displayProducts(products)
+  function fetchBlob(product) {
+    url = `images/${product.image}`;
+    
+    fetch(url).then(response => {
+      return response.blob();
+    }).then( blob => {
+      let objectURL = URL.createObjectURL(blob);
+      showProduct(objectURL, product);
+    })
+  }
 
-  // 
+  function showProduct(objectURL, product) {
+    console.log(objectURL, product);
+  }
+
+  // function displayProducts(products) {
+
+  //   products.forEach(product => {
+  //     const section = document.createElement('section');
+  //     const h2 = document.createElement('h2');
+  //     const p = document.createElement('p');
+  //     const img = document.createElement('img');
+
+  //     section.className = product.type;
+  //     h2.textContent = product.name;
+  //     p.textContent = `$${product.price}`;
+  //     img.src = `images/${product.image}`
+
+  //     productContainer.append(section);
+  //     section.append(h2);
+  //     section.append(p);
+  //     section.append(img);
+  //   })
+  // }
 }
