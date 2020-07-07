a13cddcc56af1234d88c87d74318fef3907cce2c
Joe Webster <joseph.scott.webster@gmail.com>
2020-07-07 12:13:26 -0400

    Display products

--- script.js
+++ script.js
@@ -12,6 +12,29 @@ function initialize(products) {
   const searchTerm = document.querySelector('#searchTerm');
   const productContainer = document.querySelector('.product-container');
 
+  const selectedMacro = macroSelect.value;
+
+  function displayProducts(products) {
+
+    products.forEach(product => {
+      const section = document.createElement('section');
+      const h2 = document.createElement('h2');
+      const p = document.createElement('p');
+      const img = document.createElement('img');
+
+      section.className = product.type;
+      h2.textContent = product.name;
+      p.textContent = `$${product.price}`;
+      img.src = `images/${product.image}`
+
+      productContainer.append(section);
+      section.append(h2);
+      section.append(p);
+      section.append(img);
+    })
+  }
+
+  displayProducts(products)
 
   // 
 }
