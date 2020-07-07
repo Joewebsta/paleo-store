b4abb2df199bb5138c81b85585f6293ba31718f8
Joe Webster <joseph.scott.webster@gmail.com>
2020-07-07 13:28:03 -0400

    Create showProduct function

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-fetch('products.json').then( response => {
+fetch('products.json').then(response => {
   return response.json();
-}).then( json => {
+}).then(json => {
   const products = json;
   initialize(products);
-}).catch( error => {
+}).catch(error => {
   console.log(`Fetch error: ${error.message}`)
 })
 
@@ -25,36 +25,30 @@ function initialize(products) {
 
   function fetchBlob(product) {
     url = `images/${product.image}`;
-    
+
     fetch(url).then(response => {
       return response.blob();
-    }).then( blob => {
+    }).then(blob => {
       let objectURL = URL.createObjectURL(blob);
       showProduct(objectURL, product);
     })
   }
 
   function showProduct(objectURL, product) {
-    console.log(objectURL, product);
+    const section = document.createElement('section');
+    const heading = document.createElement('h2');
+    const para = document.createElement('p');
+    const image = document.createElement('img');
+
+    section.className = product.type;
+    heading.textContent = product.name[0].toUpperCase() + product.name.slice(1);
+    para.textContent = `$${product.price.toFixed(2)}`;
+    image.src = objectURL;
+    image.alt = product.name;
+
+    productContainer.append(section);
+    section.append(heading);
+    section.append(para);
+    section.append(image);
   }
-
-  // function displayProducts(products) {
-
-  //   products.forEach(product => {
-  //     const section = document.createElement('section');
-  //     const h2 = document.createElement('h2');
-  //     const p = document.createElement('p');
-  //     const img = document.createElement('img');
-
-  //     section.className = product.type;
-  //     h2.textContent = product.name;
-  //     p.textContent = `$${product.price}`;
-  //     img.src = `images/${product.image}`
-
-  //     productContainer.append(section);
-  //     section.append(h2);
-  //     section.append(p);
-  //     section.append(img);
-  //   })
-  // }
 }
